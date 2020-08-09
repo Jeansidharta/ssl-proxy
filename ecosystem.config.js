78fd1496f6d3a8fffc317dbf8dfe112668dd9caa
jean sidharta <jeansidharta@gmail.com>
2020-08-09 15:52:33 -0300

    added pm2 config file

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+// This is a config file for the process manages `pm2`. See this link for more info:
+// https://pm2.keymetrics.io/docs/usage/application-declaration/
+
+module.exports = {
+	apps: [{
+		name: 'ssl-proxy',
+		script: 'out/index.js',
+		watch: false,
+		cwd: require.main.id,
+	}],
+};
